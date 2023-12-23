b728a1a11e32e3687de76704075a143760968cbe
vedantmishra69 <122669445+vedantmishra69@users.noreply.github.com>
2023-12-23 17:53:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import settings
 import requests
 from PIL import Image
 import os
+from ratelimit import limits, RateLimitException, sleep_and_retry
 
 
 intents = discord.Intents.default()
@@ -18,9 +19,12 @@ intents.message_content = True
 intents.members = True
 client = discord.Client(intents=intents)
 
-allowed_channels = [1187382039616430250, 1186688322853146676]
+allowed_channels = []
+allowed_guilds = [1063393625049923687, 501032525284769842]
+dev_id = 501026469569363988
 image_types = ["jpeg", "png", "webp", "heic", "heif"]
-no_response_message = "Sorry, can't answer that. possible reasons might be recitation, safety issue or blocked content."
+no_response_message = "Sorry, can't answer that. possible reasons might be no response, recitation, safety issue or blocked content."
+help_text = "```\nUsage:\n$chat : to create a public thread to interact with the bot.\n$private : to create a private thread to interact with the bot.\n\nACD AI answers to all text, image and attachment.\nHave fun!\n\nfor Admin/Mods:\n$set : to set a channel.\n$unset : to unset a channel.\n$show : to show current channels.\n$clear : to delete all the threads in set channels.```"
 API_KEY = os.environ["API_KEY"]
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 
@@ -33,9 +37,13 @@ image_model = genai.GenerativeModel(model_name="gemini-pro-vision",
                               generation_config=settings.image_generation_config, # type: ignore
                               safety_settings=settings.image_safety_settings)
 
-# chat = text_model.start_chat(history=[])
 chats = {}
 
+ONE_MINUTE = 60
+MAX_CALLS_PER_MINUTE = 60
+
+@sleep_and_retry
+@limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
 async def get_response(message, chat):
     async with message.channel.typing():
         image = None
@@ -94,38 +102,98 @@ async def print_response(response, message):
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-    client.loop.create_task(task())
 
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith("$help") and message.channel.id in allowed_channels:
-        await message.channel.send("```\n$chat -> for public chat.\n$private -> for private chat.\n```")
-    if message.content.startswith("$chat") and message.channel.id in allowed_channels:
-        thread = await message.channel.create_thread(name = "Session with " + message.author.name, slowmode_delay = 1, auto_archive_duration = 60, message = message)
-        chats[str(thread.id)] = text_model.start_chat(history=[])
-    elif message.content.startswith("$private") and message.channel.id in allowed_channels:
-        thread = await message.channel.create_thread(name = "Session with " + message.author.name, slowmode_delay = 1, auto_archive_duration = 60)
+    if message.content.startswith("$show"):
+        if has_permissions(message):
+            set_channels = []
+            count = 1
+            for Id in allowed_channels:
+                if message.guild.get_channel(Id):
+                    set_channels.append(f"\n{count}. {message.guild.get_channel(Id).name}")
+                    count += 1
+            if set_channels:
+                await message.channel.send("Current channels:" + "".join(set_channels))
+            else:
+                await message.channel.send("No channel has been set. type $set to set it.")
+        else: 
+            await message.channel.send("you don't have ``Manage channels`` permission!")
+            
+    if message.content.startswith("$set"):
+        if has_permissions(message):
+            if message.channel.type == discord.ChannelType.text:
+                if message.channel.id not in allowed_channels:
+                    allowed_channels.append(message.channel.id)
+                    await message.channel.send(f"channel ``{message.channel.name}`` has been set successfully.")
+                else:
+                    await message.channel.send(f"``{message.channel.name}`` is already set.")
+            else:
+                await message.channel.send("It should be a text channel!")
+        else:
+            await message.channel.send("you don't have ``Manage channels`` permission!")
+            
+    if message.content.startswith("$help"):
+        await message.channel.send(help_text)
+
+    if message.content.startswith("$unset"):
+        if has_permissions(message):
+            if message.channel.id in allowed_channels:
+                allowed_channels.remove(message.channel.id)
+                await message.channel.send(f"channel ``{message.channel.name}`` has been unset successfully.")
+            else:
+                await message.channel.send("This channel has not been set. type $set to set it.")
+        else:
+            await message.channel.send("you don't have ``Manage channel`` permission!")
+            
+    if message.content.startswith("$clear"):
+        if message.author.guild_permissions.manage_threads:
+            for Id in allowed_channels:
+                if message.guild.get_channel(Id):
+                    count = 0
+                    async with message.channel.typing():
+                        for thread in message.guild.get_channel(Id).threads:
+                            await thread.delete()
+                            count += 1
+                    await message.channel.send(f"{count} threads deleted for ``{message.guild.get_channel(Id).name}``.")
+        else: 
+            await message.channel.send("you don't have ``Manage threads`` permission!")
+            
+            
+    if str(message.channel.id) in chats:
+        client.loop.create_task(get_response(message, chats[str(message.channel.id)])) # type: ignore
+    elif message.content:
+        first = message.content.split()[0]
+        if first == "$chat": 
+            if message.channel.id in allowed_channels: 
+                await start_thread(message, True)
+            else:
+                await message.channel.send("This channel has not been set. type $set to set it.")
+        elif first == "$private": 
+            if message.channel.id in allowed_channels: 
+                await start_thread(message, False)
+            else:
+                await message.channel.send("This channel has not been set. type $set to set it.")
+                
+def has_permissions(message):
+    return (message.author.guild_permissions.manage_channels and message.guild.id in allowed_guilds) or message.author.id == dev_id
+
+
+async def start_thread(message, is_public):
+    try:
+        thread = await message.channel.create_thread(name = "Session with " + message.author.name, slowmode_delay = 1, auto_archive_duration = 60, message = message if is_public else None)
         await thread.add_user(message.author)
-        chats[str(thread.id)] = text_model.start_chat(history=[])
-    elif str(message.channel.id) in chats:
-        client.loop.create_task(get_response(message, chats[str(message.channel.id)]))
-    
-
-async def task():
-    while True:
-        local_time = time.localtime()
-        if [local_time.tm_wday, local_time.tm_hour] == [6, 4]:
-            for id in allowed_channels:
-                channel = client.get_channel(id)
-                async for thread in channel.archived_threads(private = True):
-                    chats.pop(thread.id)
-                    thread.delete()
-        await asyncio.sleep(1)
-    
-    
-
-      
+        hello_text = text_model.generate_content("hi").text
+        async with thread.typing():
+            await thread.send(content = f'<@{message.author.id}> {hello_text}')
+        chats[str(thread.id)] = text_model.start_chat(history=[{'role':'user', 'parts': ["hi"]}, {'role': 'model', 'parts': [hello_text]}])
+    except Exception as e:
+        await message.channel.send("Could not start a thread :(")
+        print(e)
+   
 client.run(BOT_TOKEN) 
+
+
